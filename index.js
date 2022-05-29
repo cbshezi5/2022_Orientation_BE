3593091a6e5375e281c010fb694290107ec80858
Cebolenkosi Shezi <cbshezi5@gmail.com>
2022-05-29 22:49:44 +0200

    lastedst

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var app = require('express')();
 var http = require('http').Server(app);
+const cors = require('cors')
 var io = require('socket.io')(http);
+
+app.use(cors())
 
 app.get('/', function(req, res){ res.send("Something");
 });
@@ -14,6 +17,7 @@ io.on('connection', function(socket){
       console.log('A user disconnected');
    });
 });
-http.listen(5000, function(){
-   console.log('listening on *:5000');
+var port = process.env.PORT || 5000;
+http.listen(port, function(){
+   console.log('listening on *:'+port+'');
 });
