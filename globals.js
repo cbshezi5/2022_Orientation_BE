4231e7e17f4c2f5716d653c120e687e379a47140
Cebolenkosi Shezi <cbshezi5@gmail.com>
2022-06-29 14:04:03 +0200

    Update globals.js

--- globals.js
+++ globals.js
@@ -3,7 +3,7 @@ const USERNAME = "admin";
 const PASSWORD = "icep2020"
 const DATABASE_HOSTNAME = "inst-orientation-db.cvqpj5ith2h1.us-east-1.rds.amazonaws.com";
 //API PORTS
-const HOSTNAME = "http://ec2-18-234-218-224.compute-1.amazonaws.com"
+const HOSTNAME = "https://app-orientation.herokuapp.com"
 const SOCKETIO_PORT = process.env.PORT || 8000;
 const GRAPH_PORT = process.env.PORT || 8000;
 const APP_PORT = process.env.PORT || 8000;
