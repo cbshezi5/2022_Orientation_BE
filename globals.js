120abdb6522e19d4238faeb7d8604c655c9ab0ca
Cebolenkosi Shezi <cbshezi5@gmail.com>
2022-06-29 13:36:10 +0200

    Update globals.js

--- globals.js
+++ globals.js
@@ -4,9 +4,9 @@ const PASSWORD = "icep2020"
 const DATABASE_HOSTNAME = "inst-orientation-db.cvqpj5ith2h1.us-east-1.rds.amazonaws.com";
 //API PORTS
 const HOSTNAME = "http://ec2-18-234-218-224.compute-1.amazonaws.com"
-const SOCKETIO_PORT = process.env.PORT || 80;
-const GRAPH_PORT = process.env.PORT || 80;
-const APP_PORT = process.env.PORT || 80;
+const SOCKETIO_PORT = process.env.PORT || 8000;
+const GRAPH_PORT = process.env.PORT || 8000;
+const APP_PORT = process.env.PORT || 8000;
 
 
 function DATABASE()
